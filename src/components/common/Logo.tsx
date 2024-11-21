daa480e7b0bcc043e67235d3763a53665c9dacbc
Sirri Demirtas <sirridemirtas@gmail.com>
2024-11-21 19:05:54 +0300

    Add Logo component with customizable className and responsive design

---
+++ src/components/common/Logo.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import cn from "classnames";
+import { Origami } from "lucide-react";
+
+interface LogoProps {
+  className?: string;
+}
+
+const Logo: React.FC<LogoProps> = ({ className }) => {
+  return (
+    <div
+      className={cn(
+        "w-12 h-12 rounded-full flex items-center justify-center",
+        "transition-colors duration-300",
+        "hidden lg:flex cursor-pointer",
+        //"text-blue-700 bg-blue-50",
+        className
+      )}
+    >
+      <Origami size={28} />
+    </div>
+  );
+};
+
+export default Logo;

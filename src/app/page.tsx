1c415d1341a33f0118f46c020f6f2d0e41416fd8
Sirri Demirtas <sirridemirtas@gmail.com>
2024-11-21 19:19:46 +0300

    Fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,12 @@ import Post from "@/components/common/Post";
 import { House } from "lucide-react";
 import { MOCK_POSTS } from "@/constants/posts";
 import { useAuth } from "@/hooks/useAuth";
-import DialogExample from "@/components/ui/DialogExample";
 
 export default function Home() {
   const { isLoggedIn } = useAuth();
   return (
     <div className="">
       <PageTitle title="Ana Sayfa" icon={House} />
-      {/* <DialogExample /> */}
       {isLoggedIn && <CreatePost />}
       <div className="sm:space-y-4">
         {MOCK_POSTS.map((post) => (
